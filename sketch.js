bdeee9c4a919bc5ff93b04078f0be1872c407626
jcluq <jc.luque10@uniandes.edu.co>
2020-12-02 16:24:26 -0500

    Ultimos

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let escala = 0.4
+let escala = .4
 let botones = []
 let ancho
 let alto
@@ -17,7 +17,7 @@ function preload(){
   s[5] = loadSound("sonidos/6Yataro.wav")
   s[6] = loadSound("sonidos/7Strunella.wav")
   s[7] = loadSound("sonidos/8Quenquen.wav")
-
+  s[8] = loadSound("sonidos/9Don.wav")
   
 }
 
@@ -31,27 +31,31 @@ function setup() {
   image(mapa,0,0,width, height)
   
   
-  botones[0] = new Boton(width * 0.0360,height*0.174,s[0]); //Paisaje Sonoro
-  botones[1] = new Boton(width * 0.1925,height*0.216,s[1],"imagenes/mTangara.png"); //Tangata 
-  botones[2] = new Boton(width * 0.6231,height*0.194,s[2],"imagenes/mCompeton.png");//Copeton
-  botones[3] = new Boton(width * 0.3535,height*0.665,s[3]); //Vaca
-  botones[4] = new Boton(width * 0.4838,height*0.564,s[4],"imagenes/mCardenal.png"); //Cardinal
-  botones[5] = new Boton(width * 0.5250,height*0.721,s[5],"imagenes/mYataro.png"); //Yataro
-  botones[6] = new Boton(width * 0.6395,height*0.514,s[6],"imagenes/mStrunella.png"); //Sturnella
-  botones[7] = new Boton(width * 0.9432,height*0.3275,s[7],"imagenes/mQuenquen.png"); //Quenquen
-  
+  botones[0] = new Boton(width * 0.0360,height*0.174,s[0],"","Paisaje Sonoro"); //Paisaje Sonoro
+  botones[1] = new Boton(width * 0.1925,height*0.216,s[1],"imagenes/mTangara.png","Tangara Vassorii"); //Tangata 
+  botones[2] = new Boton(width * 0.6231,height*0.194,s[2],"imagenes/mCompeton.png", "Zonitrichia Capensis");//Copeton
+  botones[3] = new Boton(width * 0.3535,height*0.665,s[3],"","Vaca"); //Vaca
+  botones[4] = new Boton(width * 0.4838,height*0.564,s[4],"imagenes/mCardenal.png","Piranga Rubra"); //Cardinal
+  botones[5] = new Boton(width * 0.5250,height*0.721,s[5],"imagenes/mYataro.png"," Aulacorhynchus Albivitta"); //Yataro
+  botones[6] = new Boton(width * 0.6395,height*0.514,s[6],"imagenes/mStrunella.png","Sturnella Magna"); //Sturnella
+  botones[7] = new Boton(width * 0.9432,height*0.3275,s[7],"imagenes/mQuenquen.png","Cyanocorax Yncas"); //Quenquen
+  botones[8] = new Boton(width * 0.6410,height*0.0428,s[8],"imagenes/Don.png","Don Mayorga");
+
  
 }
 
 
 
 function draw() {
+  image(mapa,0,0,width, height)
   cursor(CROSS)
+
   
   botones.forEach(function(boton) {
     boton.pintar();
     if (boton.mouseIn()){
       boton.imagenCursor()
+      boton.dispNombre()
     }
   })
 
@@ -71,17 +75,20 @@ function mouseClicked(){
 }
 
 class Boton{
-  constructor(x,y,s,ic){
+  constructor(x,y,s,ic,nom){
   this.x = x;
   this.y = y;
   this.d = 62*escala;
   this.sonido = s
   this.ic = ic
+  this.nom = nom
   }
   
   pintar(){
-    noFill(255,0,0)
+    stroke(0)
+    noFill();
     ellipse(this.x,this.y,this.d)
+    fill(0)
   }
   
   mouseIn(){
@@ -100,6 +107,20 @@ class Boton{
   }
   imagenCursor(){
     cursor(this.ic)
+  }
+
+  dispNombre(){
+    textSize(18)
+    let ancua = textWidth(this.nom)
+    let altcua = textAscent() + textDescent();
+    fill(255,200)
+    noStroke()
+    rect(mouseX+5-ancua/2,mouseY-5-altcua,ancua+10, altcua)
+    
+    fill(0)
+    text(this.nom,mouseX+10-ancua/2,mouseY-10)
+
+
   }
   
 }
